9a6f65cc56fb0c5db0a109cb5af9ccef450179b1
DagmawiAbate <dagmawiabate2@gmail.com>
2022-09-16 11:31:10 +0300

    React route setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,29 @@
 import React, { Fragment } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Landing from './components/layout/Landing'
+import Register from './components/auth/Register'
+import Login from './components/auth/Login'
+import NoMatch from './components/NoMatch'
 import './App.css'
 
 const App = () => (
-  <Fragment>
-    <Navbar />
-    <Landing />
-  </Fragment>
+  <Router>
+    <Fragment>
+      <Navbar />   
+      <Routes>
+        <Route exact path='/' element={<Landing />} />
+        <Route path='*' element={<NoMatch />} />
+      </Routes>
+      <section className='container'>
+        <Routes>
+          <Route exact path='/register' element={<Register />} />
+          <Route exact path='/login' element={<Login />} />
+          <Route path='*' element={<NoMatch />} />
+        </Routes>
+      </section>
+    </Fragment>
+  </Router>
 )
 
 export default App
