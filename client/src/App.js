07fbb6a310c1d265c1b3190e608dd8c1730692dd
DagmawiAbate <dagmawiabate2@gmail.com>
2022-09-24 17:05:29 +0300

    Alert component & action call

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/layout/Navbar'
 import Landing from './components/layout/Landing'
 import Register from './components/auth/Register'
 import Login from './components/auth/Login'
+import Alert from './components/layout/Alert'
 import NoMatch from './components/NoMatch'
 // Redux
 import { Provider } from 'react-redux'
@@ -21,6 +22,7 @@ const App = () => (
           <Route path='*' element={<NoMatch />} />
         </Routes>
         <section className='container'>
+          <Alert />
           <Routes>
             <Route exact path='/register' element={<Register />} />
             <Route exact path='/login' element={<Login />} />
