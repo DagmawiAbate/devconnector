53c99d8673b36ce18e80bbd3af26ce41996c909f
DagmawiAbate <dagmawiabate2@gmail.com>
2022-09-29 13:04:52 +0300

    Bug-fix: user login

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -66,7 +66,7 @@ Login.propTypes = {
 }
 
 const mapStateToProps = (state) => ({
-  isAuthenticated: state.auth.isAuthentcated
+  isAuthenticated: state.auth.isAuthenticated
 })
 
 export default connect(mapStateToProps, { login })(Login)
