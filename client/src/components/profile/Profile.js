9926f9b820a4add5c68b1dba0ef4d3127e7e4349
DagmawiAbate <dagmawiabate2@gmail.com>
2022-10-26 10:54:12 +0300

    ProfileTop and ProfileAbout components

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types'
 import { Link, useParams } from 'react-router-dom'
 import { connect } from 'react-redux'
 import Spinner from '../layout/Spinner'
+import ProfileTop from './ProfileTop'
+import ProfileAbout from './ProfileAbout'
 import { getProfileById } from '../../actions/profile'
 
 const Profile = ({ getProfileById, profile: { profile, loading }, auth }) => {
@@ -20,9 +22,17 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth }) => {
           <Link to='/profiles' className='btn btn-light'>
             Back To Profiles
           </Link>
-          {auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id && (<Link to='/edit-profile' className='btn btn-dark'>
-            Edit Profile
-          </Link>)} 
+          {auth.isAuthenticated &&
+            auth.loading === false &&
+            auth.user._id === profile.user._id && (
+              <Link to='/edit-profile' className='btn btn-dark'>
+                Edit Profile
+              </Link>
+            )}
+            <div class="profile-grid my-1">
+              <ProfileTop profile={profile}/>
+              <ProfileAbout profile={profile}/>
+            </div>
         </Fragment>
       )}
     </Fragment>
