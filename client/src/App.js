f39a5741e8bab4d16fac4f890b14c1a5772a8da5
DagmawiAbate <dagmawiabate2@gmail.com>
2022-10-28 12:33:14 +0300

    Post reducer, action & initial component

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import AddExperience from './components/profile-forms/AddExperience'
 import AddEducation from './components/profile-forms/AddEducation'
 import Profiles from './components/profiles/Profiles'
 import Profile from './components/profile/Profile'
+import Posts from './components/posts/Posts'
 import PrivateRoute from './components/routing/PrivateRoute'
 import NoMatch from './components/NoMatch'
 // Redux
@@ -66,6 +67,10 @@ const App = () => {
               <Route
                 path='/add-education'
                 element={<PrivateRoute component={AddEducation} />}
+              />
+              <Route
+                path='/posts'
+                element={<PrivateRoute component={Posts} />}
               />
               <Route path='*' element={<NoMatch />} />
             </Routes>
