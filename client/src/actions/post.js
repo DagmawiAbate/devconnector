a4fb6097d42286f613839756f940f3cffb174c1f
DagmawiAbate <dagmawiabate2@gmail.com>
2022-11-02 15:03:49 +0300

    Single post display

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,13 @@
 import axios from 'axios'
 import { setAlert } from './alert'
-import { ADD_POST, DELETE_POST, GET_POSTS, POST_ERROR, UPDATE_LIKES } from './types'
+import {
+  ADD_POST,
+  DELETE_POST,
+  GET_POSTS,
+  POST_ERROR,
+  UPDATE_LIKES,
+  GET_POST
+} from './types'
 
 // Get posts
 export const getPosts = () => async (dispatch) => {
@@ -73,7 +80,7 @@ export const deletePost = (id) => async (dispatch) => {
 }
 
 // Add post
-export const addPost = formData => async (dispatch) => {
+export const addPost = (formData) => async (dispatch) => {
   const config = {
     headers: {
       'Content-Type': 'application/json'
@@ -89,6 +96,23 @@ export const addPost = formData => async (dispatch) => {
     })
 
     dispatch(setAlert('Post Created', 'success'))
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    })
+  }
+}
+
+// Get post
+export const getPost = (id) => async (dispatch) => {
+  try {
+    const res = await axios.get(`/api/posts/${id}`)
+
+    dispatch({
+      type: GET_POST,
+      payload: res.data
+    })
   } catch (err) {
     dispatch({
       type: POST_ERROR,
